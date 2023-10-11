bab93db57d00937f0cf4e01b08ce16daeafd4da3
lautaro <lautihernando@gmail.com>
2023-10-11 15:40:58 -0300

    add docker compose and chage migrator

--- sqlMigrator/migrator.sh
+++ sqlMigrator/migrator.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+docker exec $1 psql -U admin -d postgres -c "CREATE DATABASE tp2;"
+docker cp ../schema.sql $1:/tmp/schema.sql
+docker cp ../inserts.sql $1:/tmp/inserts.sql
+docker exec -u postgres $1 psql -U admin -d postgres --file /tmp/schema.sql
+docker exec -u postgres $1 psql -U admin -d postgres --file /tmp/inserts.sql
+
+
 docker cp ./productos.sql $1:/tmp/productos.sql
 docker exec -u postgres $1 psql -U admin -d postgres --file /tmp/productos.sql
-docker cp $1:/tmp/productos.json productos.json
-docker cp ./productos.json $2:/tmp/productos.json
+docker exec -it $1 mv /tmp/productos.json /tmp/migration/productos.json
 docker exec $2 mongosh mongodb://admin:admin@127.0.0.1:27017/tp2 --authenticationDatabase admin --eval "db.producto.drop()"
-docker exec $2 mongoimport -d tp2 --authenticationDatabase admin --username admin --password admin -c producto --file  /tmp/productos.json 
+docker exec $2 mongoimport -d tp2 --authenticationDatabase admin --username admin --password admin -c producto --file  /migration/productos.json 
 
 docker cp ./clientes.sql $1:/tmp/clientes.sql
 docker exec -u postgres $1 psql -U admin -d postgres --file /tmp/clientes.sql
-docker cp $1:/tmp/clientes.json clientes.json
-docker cp ./clientes.json $2:/tmp/clientes.json
+docker exec -it $1 mv /tmp/cliente.json /tmp/migration/cliente.json
 docker exec $2 mongosh mongodb://admin:admin@127.0.0.1:27017/tp2 --authenticationDatabase admin --eval "db.cliente.drop()"
-docker exec $2 mongoimport -d tp2 --authenticationDatabase admin --username admin --password admin -c cliente --file  /tmp/clientes.json 
+docker exec $2 mongoimport -d tp2 --authenticationDatabase admin --username admin --password admin -c cliente --file  /migration/cliente.json 
 
 docker cp ./factura.sql $1:/tmp/factura.sql
 docker exec -u postgres $1 psql -U admin -d postgres --file /tmp/factura.sql
-docker cp $1:/tmp/factura.json factura.json
-docker cp ./factura.json $2:/tmp/factura.json
+docker exec -it $1 mv /tmp/factura.json /tmp/migration/factura.json
 docker exec $2 mongosh mongodb://admin:admin@127.0.0.1:27017/tp2 --authenticationDatabase admin --eval "db.factura.drop()"
-docker exec $2 mongoimport -d tp2 --authenticationDatabase admin --username admin --password admin -c factura --file  /tmp/factura.json 
+docker exec $2 mongoimport -d tp2 --authenticationDatabase admin --username admin --password admin -c factura --file  /migration/factura.json 
 
 docker cp ./telefono.sql $1:/tmp/telefono.sql
 docker exec -u postgres $1 psql -U admin -d postgres --file /tmp/telefono.sql
-docker cp $1:/tmp/telefono.json telefono.json
-docker cp ./telefono.json $2:/tmp/telefono.json
+docker exec -it $1 mv /tmp/telefono.json /tmp/migration/telefono.json
 docker exec $2 mongosh mongodb://admin:admin@127.0.0.1:27017/tp2 --authenticationDatabase admin --eval "db.telefono.drop()"
-docker exec $2 mongoimport -d tp2 --authenticationDatabase admin --username admin --password admin -c telefono --file  /tmp/telefono.json 
-
-rm -r *.json
+docker exec $2 mongoimport -d tp2 --authenticationDatabase admin --username admin --password admin -c telefono --file  /migration/telefono.json 
