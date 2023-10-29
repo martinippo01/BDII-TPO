d53aec32db4d1f4d2d3f87e7f85588d4deeef2da
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2023-10-28 21:21:47 -0300

    Fix API only return 1 line

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const pool = new Pool({
   host: "localhost",
   database: "postgres",
   password: "admin",
-  port: 5432,
+  port: 5433,
   // Additional options
   idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
   max: 10, // Limit the maximum number of connections
@@ -80,7 +80,7 @@ app.get("/clients/:id", async (req, res) => {
     if (result.rowCount === 0) {
       res.status(404).json({ error: "User not found" }); // 404 Not Found
     } else {
-      res.status(200).json(result.rows[0]);
+      res.status(200).json(result.rows);
     }
   }catch(error){
     console.error(error);
@@ -101,7 +101,7 @@ app.put("/clients/:id", async (req, res) => {
     if (result.rowCount === 0) {
       res.status(404).json({ error: "User not found" }); // 404 Not Found
     } else {
-      res.status(200).json(result.rows[0]);
+      res.status(200).json(result.rows);
     }
   } catch (error) {
     console.error("Error updating user:", error);
@@ -147,7 +147,7 @@ app.get("/products/:id", async (req, res) => {
     if (result.rowCount === 0) {
       res.status(404).json({ error: "Product not found" }); // 404 Not Found
     } else {
-      res.status(200).json(result.rows[0]);
+      res.status(200).json(result.rows);
     }
   }catch(error){
     console.error(error);
@@ -162,7 +162,7 @@ app.post("/products", async (req, res) => {
       "INSERT INTO e01_producto (marca,nombre,descripcion,precio,stock) values ($1, $2,$3,$4,$5) RETURNING *",
       [marca,nombre,descripcion,precio,stock]
     );
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to save product" });
@@ -180,7 +180,7 @@ app.put("/products/:id", async (req, res) => {
     if (result.rowCount === 0) {
       res.status(404).json({ error: "Product not found" }); // 404 Not Found
     } else {
-      res.status(200).json(result.rows[0]);
+      res.status(200).json(result.rows);
     }
   }catch(error){
     console.error(error);
@@ -211,7 +211,7 @@ app.delete("/products/:id", async (req, res) => {
 app.get("/queries/1", async (req, res) => {
   try{
     const result = await pool.query("SELECT E01_CLIENTE.nro_cliente, codigo_area, nro_telefono FROM E01_CLIENTE JOIN E01_TELEFONO ON E01_CLIENTE.nro_cliente = E01_TELEFONO.nro_cliente WHERE E01_CLIENTE.nombre = 'Wanda' AND E01_CLIENTE.apellido = 'Baker';");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -222,7 +222,7 @@ app.get("/queries/1", async (req, res) => {
 app.get("/queries/2", async (req, res) => {
   try{
     const result = await pool.query("SELECT DISTINCT E01_CLIENTE.nro_cliente, E01_CLIENTE.nombre, E01_CLIENTE.apellido FROM E01_CLIENTE JOIN E01_FACTURA ON E01_CLIENTE.nro_cliente = E01_FACTURA.nro_cliente;");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -233,7 +233,7 @@ app.get("/queries/2", async (req, res) => {
 app.get("/queries/3", async (req, res) => {
   try{
     const result = await pool.query("SELECT E01_CLIENTE.nro_cliente, E01_CLIENTE.nombre, E01_CLIENTE.apellido FROM E01_CLIENTE LEFT JOIN E01_FACTURA ON E01_CLIENTE.nro_cliente = E01_FACTURA.nro_cliente WHERE E01_FACTURA.nro_factura IS NULL;");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -244,7 +244,7 @@ app.get("/queries/3", async (req, res) => {
 app.get("/queries/4", async (req, res) => {
   try{
     const result = await pool.query("SELECT DISTINCT E01_PRODUCTO.codigo_producto, E01_PRODUCTO.marca, E01_PRODUCTO.nombre FROM E01_PRODUCTO JOIN E01_DETALLE_FACTURA ON E01_PRODUCTO.codigo_producto = E01_DETALLE_FACTURA.codigo_producto;");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -255,7 +255,7 @@ app.get("/queries/4", async (req, res) => {
 app.get("/queries/5", async (req, res) => {
   try{
     const result = await pool.query("select nro_cliente as \"Numero de cliente\", concat(apellido, ',', nombre) as \"Appelido, Nombre\", tipo as \"Sexo\", direccion as \"Direccion\", activo as \"Activo\", concat('(',codigo_area,') ', nro_telefono) as \"Numero de telefono\" from e01_cliente natural join e01_telefono;");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -266,7 +266,7 @@ app.get("/queries/5", async (req, res) => {
 app.get("/queries/6", async (req, res) => {
   try{
     const result = await pool.query("select c.nro_cliente as \"Numero de cliente\", concat(apellido, ',', nombre) as \"Appelido, Nombre\", cant_fact as \"Cantidad de facturas\" from e01_cliente as c join (select nro_cliente, count(*) as cant_fact from e01_factura group by nro_cliente) as f on f.nro_cliente = c.nro_cliente;");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -277,7 +277,7 @@ app.get("/queries/6", async (req, res) => {
 app.get("/queries/7", async (req, res) => {
   try{
     const result = await pool.query("select c.nro_cliente as \"Numero de cliente\", concat(apellido, ',', nombre) as \"Appelido, Nombre\", nro_factura as \"Numero de factura\", fecha as \"Fecha\", total_sin_iva as \"Monto (sin iva)\", iva as \"IVA\", total_con_iva as \"Monto (con iva)\" from e01_factura f natural join (select nombre, apellido, nro_cliente from e01_cliente where lower(apellido) = lower('Tate') and lower(nombre) = lower('Pandora')) c;");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -288,7 +288,7 @@ app.get("/queries/7", async (req, res) => {
 app.get("/queries/8", async (req, res) => {
   try{
     const result = await pool.query("select distinct(nro_factura) from e01_detalle_factura natural join e01_producto where marca = 'In Faucibus Inc.'");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -299,7 +299,7 @@ app.get("/queries/8", async (req, res) => {
 app.get("/queries/9", async (req, res) => {
   try{
     const result = await pool.query("select nro_telefono,nombre,apellido, direccion,activo from e01_cliente natural join e01_telefono");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -310,7 +310,7 @@ app.get("/queries/9", async (req, res) => {
 app.get("/queries/10", async (req, res) => {
   try{
     const result = await pool.query("select nombre,apellido,sum(total_con_iva) from e01_cliente natural join e01_factura group by nombre, apellido");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -355,7 +355,7 @@ app.post("/view/2", async (req, res) => {
 app.get("/view/1", async (req, res) => {
   try{
     const result = await pool.query("select * from E01_FACTURA_ORDENADAS_POR_FECHA;");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
@@ -365,7 +365,7 @@ app.get("/view/1", async (req, res) => {
 app.get("/view/2", async (req, res) => {
   try{
     const result = await pool.query("select * from PRODUCTOS_NO_FACTURADOS;");
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   }catch(error){
     console.error(error);
     res.status(500).json({ error: "Failed to retrieve product" });
